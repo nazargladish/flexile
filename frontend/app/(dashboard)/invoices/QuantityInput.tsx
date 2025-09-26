98f69e102397700e7ffb2171b4057090a6a251f8
nazar <gladyshinbox@outlook.com>
2025-09-26 18:18:54 +0200

    Fix invoices complete-flow E2E by updating quantity input state onChange instead of onBlur

--- frontend/app/(dashboard)/invoices/QuantityInput.tsx
+++ frontend/app/(dashboard)/invoices/QuantityInput.tsx
@@ -13,31 +13,34 @@ const QuantityInput = ({
   onChange: (value: Value) => void;
 } & Omit<React.ComponentProps<"input">, "value" | "onChange">) => {
   const [rawValue, setRawValue] = useState("");
-  useEffect(
-    () => setRawValue(value ? (value.hourly ? formatDuration(value.quantity) : value.quantity.toString()) : ""),
-    [value],
-  );
+  const [focused, setFocused] = useState(false);
 
-  return (
-    <Input
-      {...props}
-      value={rawValue}
-      onChange={(e) => setRawValue(e.target.value)}
-      onBlur={() => {
-        if (!rawValue.length) return onChange(null);
+  useEffect(() => {
+    if (focused) return;
+    setRawValue(value ? (value.hourly ? formatDuration(value.quantity) : value.quantity.toString()) : "");
+  }, [value]);
 
-        const valueSplit = rawValue.split(":");
-        if (valueSplit.length === 1) return onChange({ quantity: parseFloat(valueSplit[0] ?? "0"), hourly: false });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setRawValue(input);
 
-        const hours = parseFloat(valueSplit[0] ?? "0");
-        const minutes = parseFloat(valueSplit[1] ?? "0");
-        onChange({
-          quantity: Math.floor(isNaN(hours) ? 0 : hours * 60) + (isNaN(minutes) ? 0 : minutes),
-          hourly: true,
-        });
-      }}
-    />
-  );
+    if (!input.length) return onChange(null);
+
+    const valueSplit = input.split(":");
+    if (valueSplit.length === 1) return onChange({ quantity: parseFloat(valueSplit[0] ?? "0"), hourly: false });
+
+    const hours = parseFloat(valueSplit[0] ?? "0");
+    const minutes = parseFloat(valueSplit[1] ?? "0");
+    onChange({
+      quantity: Math.floor(isNaN(hours) ? 0 : hours * 60) + (isNaN(minutes) ? 0 : minutes),
+      hourly: true,
+    });
+  };
+
+  const handleFocus = () => setFocused(true);
+  const handleBlur = () => setFocused(false);
+
+  return <Input {...props} value={rawValue} onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur} />;
 };
 
 export default QuantityInput;
